ba2f8430199c72084d3123e7844974992beba3ce
Anthony <77572391+axiang719@users.noreply.github.com>
2021-12-05 23:52:06 -0700

    Create Lexan.h

---
+++ HW5/Lexan.h
@@ -0,0 +1,34 @@
+#ifndef LEXAN_H_INCLUDE
+#define LEXAN_H_INCLUDE
+#include <string>       // for use of strings 
+#include <vector>       // for lex vector
+#include <iostream>
+#include <sstream>     // for use of stringstream
+#include <cctype>      // to be able to use tolower, isspace, isdigit, etc.
+#include <algorithm>   // for transform
+
+class Lexan{
+
+public:
+
+Lexan() =  delete;                                    // deleting default ctor
+Lexan(const Lexan&) = default;                        // copy ctor
+Lexan &operator= (const Lexan &) = default;           // assignment operator
+~Lexan()= default;                                   // dtor
+
+Lexan(const std::string&);                          // declaring ctor with const string ref. parameters
+[[nodiscard]] size_t size() const ;                 // const size() method with nodiscard attribute
+[[nodiscard]]bool empty() const;                    // const empty() method with nodiscard attribute
+void clear();                                       // clear method
+std::string operator[](const size_t) const;         // const [size_t n] method
+
+private:
+
+std::vector <std::string> lex;                      // declared a string vector to hold tokes of type string 
+//---------------------------------------------------- use of HW1 functions 
+void skip_White_Comments( std::string & ) const;
+bool start_with(const std:: string &, const std::string &) const;
+};
+
+std::ostream &operator << (std::ostream &, const Lexan&);
+#endif /* LEXAN_H_INCLUDE */
